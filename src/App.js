cd78ac4d2e3f2efbee91c52e4de700911526355c
jwr9495 <skdlwn156@naver.com>
2019-11-19 20:36:50 +0900

    master gogo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons.cjs";
+import "./css/App.css";
 
 import Login from "./components/Login";
 import Home from "./components/Home";
@@ -10,208 +11,71 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Post from "./components/Post";
 
-const url = (name, wrap = false) =>
-  `${
-    wrap ? "url(" : ""
-  }https://awv3node-homepage.surge.sh/build/assets/${name}.svg${
-    wrap ? ")" : ""
-  }`;
+const Page = ({ offset, caption, first, second, gradient, onClick }) => (
+  <React.Fragment>
+    <Parallax.Layer offset={offset} speed={0.2} onClick={onClick}>
+      <div className="slopeBegin" />
+    </Parallax.Layer>
+
+    <Parallax.Layer offset={offset} speed={-0.2} onClick={onClick}>
+      <div className={`slopeEnd ${gradient}`} />
+    </Parallax.Layer>
+
+    <Parallax.Layer className="text number" offset={offset} speed={0.3}>
+      <span>0{offset + 1}</span>
+    </Parallax.Layer>
+
+    <Parallax.Layer className="text header" offset={offset} speed={0.4}>
+      <span>
+        <p style={{ fontSize: 20 }}>{caption}</p>
+        <div className={`stripe ${gradient}`} />
+        <p>{first}</p>
+        <p>{second}</p>
+      </span>
+    </Parallax.Layer>
+  </React.Fragment>
+);
 
 export default class App extends Component {
+  scroll = to => this.DOMnode.scrollTo(to);
+  getRef = node => {
+    this.DOMnode = node;
+  };
+
   render() {
     return (
-      <Parallax horizontal={true} ref={ref => (this.parallax = ref)} pages={3}>
-        <ParallaxLayer
-          offset={1}
-          speed={1}
-          style={{ backgroundColor: "#805E73" }}
-        />
-        <ParallaxLayer
-          offset={2}
-          speed={1}
-          style={{ backgroundColor: "#87BCDE" }}
-        />
-
-        <ParallaxLayer
+      <Parallax
+        className="container"
+        ref={this.getRef}
+        pages={3}
+        horizontal
+        scrolling={true}
+      >
+        <Page
           offset={0}
-          speed={0}
-          factor={3}
-          style={{
-            backgroundImage: url("stars", true),
-            backgroundSize: "cover"
-          }}
+          gradient="pink"
+          caption="who we are"
+          first="Lorem ipsum"
+          second="dolor sit"
+          onClick={() => this.scroll(1)}
         />
-
-        <ParallaxLayer
-          offset={1.3}
-          speed={-0.3}
-          style={{ pointerEvents: "none" }}
-        >
-          <img
-            src={url("satellite4")}
-            style={{ width: "15%", marginLeft: "70%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "55%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "10%", marginLeft: "15%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "70%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "40%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "10%", marginLeft: "10%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "75%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "60%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "25%", marginLeft: "30%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "10%", marginLeft: "80%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "20%", marginLeft: "5%" }}
-          />
-          <img
-            src={url("cloud")}
-            style={{ display: "block", width: "15%", marginLeft: "75%" }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={2.5}
-          speed={-0.4}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            pointerEvents: "none"
-          }}
-        >
-          <img src={url("earth")} style={{ width: "60%" }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={2}
-          speed={-0.3}
-          style={{
-            backgroundSize: "80%",
-            backgroundPosition: "center",
-            backgroundImage: url("clients", true)
-          }}
-        />
-
-        <ParallaxLayer
-          offset={0}
-          speed={0.1}
-          onClick={() => this.parallax.scrollTo(1)}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <img src={url("server")} style={{ width: "20%" }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer
+        <Page
           offset={1}
-          speed={0.1}
-          onClick={() => this.parallax.scrollTo(2)}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <img src={url("bash")} style={{ width: "40%" }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer
+          gradient="teal"
+          caption="what we do"
+          first="consectetur"
+          second="adipiscing elit"
+          onClick={() => this.scroll(2)}
+        />
+        <Page
           offset={2}
-          speed={-0}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-          onClick={() => this.parallax.scrollTo(0)}
-        >
-          <img src={url("clients-main")} style={{ width: "40%" }} />
-        </ParallaxLayer>
+          gradient="tomato"
+          caption="what we want"
+          first="Morbi quis"
+          second="est dignissim"
+          onClick={() => this.scroll(0)}
+        />
       </Parallax>
     );
-    // state = {
-    //   x: 0,
-    //   y: 0
-    // };
-
-    // scrollHandler = () => {
-    //   this.setState({
-    //     x: window.pageXOffset,
-    //     y: window.pageYOffset
-    //   });
-    // };
-
-    // componentDidMount() {
-    //   window.addEventListener("scroll", this.scrollHandler);
-    // }
-
-    // componentWillMount() {
-    //   window.addEventListener("scroll", this.scrollHandler);
-    // }
-
-    // render() {
-    //   return (
-    //     <>
-    //       <BrowserRouter>
-    //         <Nav />
-    //         <Header />
-    //         <Route exact path="/" component={Home} />
-    //         <h1>
-    //           <div>
-    //             X : {this.state.x} Y : {this.state.y}
-    //           </div>
-    //         </h1>
-    //         <Route path="/login" component={Login} />
-    //         <Route path="/post" component={Post} />
-    //         <Footer />
-    //       </BrowserRouter>
-    //     </>
-    //   );
   }
 }
